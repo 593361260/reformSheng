6c412beb21cb5b3dc14e29ad401f54b9d6327858
zhiming <593361260@qq.com>
2020-02-26 23:49:44 +0800

    修改 ssl 过期签名

---
+++ usermodule/src/main/java/org/kevin/usermodule/injection/component/UserComponent.kt
@@ -0,0 +1,7 @@
+package org.kevin.usermodule.injection.component
+
+import dagger.Component
+
+@Component(modules = [UserConponent::class])
+interface UserConponent {
+}
---
+++ usermodule/src/main/java/org/kevin/usermodule/injection/module/RegisterModule.kt
@@ -0,0 +1,2 @@
+package org.kevin.usermodule.injection.module
+
