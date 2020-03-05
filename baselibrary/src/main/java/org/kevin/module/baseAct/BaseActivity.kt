01bbd1a4ae0982eebf41b24075cbbc85c90412ee
593361260 <593361260@qq.com>
2020-03-05 19:17:18 +0800

    添加activity 管理

--- baselibrary/src/main/java/org/kevin/module/baseAct/BaseActivity.kt
+++ baselibrary/src/main/java/org/kevin/module/baseAct/BaseActivity.kt
@@ -1,7 +1,19 @@
 package org.kevin.module.baseAct
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import org.kevin.module.comment.AppManager
 
 open class BaseActivity(layout: Int = 0) : RxAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppManager.instances.addStack(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instances.finishActivity(this)
+    }
 }
---
+++ baselibrary/src/main/java/org/kevin/module/comment/AppManager.kt
@@ -0,0 +1,40 @@
+package org.kevin.module.comment
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import java.util.*
+
+class AppManager private constructor() {
+
+    private val activityStack = Stack<Activity>()
+
+    companion object {
+        val instances = AppManager()
+    }
+
+    fun addStack(activity: Activity) {
+        activityStack.add(activity)
+    }
+
+    fun finishActivity(activity: Activity) {
+        activity.finish()
+        activityStack.remove(activity)
+    }
+
+    fun finishAllActivity() {
+        activityStack.forEach {
+            it.finish()
+        }
+        activityStack.clear()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun exitProcess(context: Context) {
+        finishAllActivity()
+        val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        manager.killBackgroundProcesses(context.packageName)
+    }
+
+}
